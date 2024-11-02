dd00321f9c265625591f94278b4d50f4035ea139
Eric Kunzel <kunzel.eric@gmail.com>
2024-11-02 17:06:58 -0300

    fix: included new properties from api response in cat types

--- src/@types/cat.ts
+++ src/@types/cat.ts
@@ -1,25 +1,49 @@
-
 export interface Weight {
-	imperial: string;
-	metric: string;
+  imperial: string;
+  metric: string;
 }
 
 export interface Breed {
-	weight: Weight;
-	id: string;
-	name: string;
-	temperament: string;
-	origin: string;
-	country_codes: string;
-	country_code: string;
-	life_span: string;
-	wikipedia_url: string;
+  weight: Weight;
+  id: string;
+  name: string;
+  vetstreet_url: string;
+  temperament: string;
+  origin: string;
+  country_codes: string;
+  country_code: string;
+  description: string;
+  life_span: string;
+  indoor: number;
+  alt_names: string;
+  adaptability: number;
+  affection_level: number;
+  child_friendly: number;
+  dog_friendly: number;
+  energy_level: number;
+  grooming: number;
+  health_issues: number;
+  intelligence: number;
+  shedding_level: number;
+  social_needs: number;
+  stranger_friendly: number;
+  vocalisation: number;
+  experimental: number;
+  hairless: number;
+  natural: number;
+  rare: number;
+  rex: number;
+  suppressed_tail: number;
+  short_legs: number;
+  wikipedia_url: string;
+  hypoallergenic: number;
+  reference_image_id: string;
 }
 
 export interface CatProps {
-	id: string;
-	width: number;
-	height: number;
-	url: string;
-	breeds: Breed[];
+  id: string;
+  width: number;
+  height: number;
+  url: string;
+  breeds: Breed[];
 }
