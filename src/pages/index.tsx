bc497230caf19bff713b841d68cac28a6d045548
Eric Kunzel <kunzel.eric@gmail.com>
2024-11-02 17:07:56 -0300

    feat: included filter pill in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,50 @@
+import { fetchCategories } from "@/api/fetch-categories";
 import { fetchCats } from "@/api/fetch-cats";
 import { Card } from "@/components/Card";
+import { FilterPill } from "@/components/FilterPill";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
-  const { data: cats, isLoading } = useQuery({
+  const { data: cats, isLoading: isLoadingCats } = useQuery({
     queryKey: ["cats"],
     queryFn: fetchCats,
   });
 
-  if (isLoading && !cats) return <>Loading...</>;
+  const { data: categories, isLoading: isLoadingCategories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  if (isLoadingCats || isLoadingCategories) return <div className="flex h-screen justify-center items-center">Loading...</div>;
 
   return (
-    <main className="mx-6">
-      <h1 className="text-2xl text-white font-bold text-center py-6">
-        CATKNOW
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-items-center gap-4">
+    <section className="mb-4 flex flex-col justify-center md:max-w-3xl lg:max-w-4xl xl:max-w-5xl">
+      <div className="flex flex-wrap justify-start gap-2 py-4">
+        {categories &&
+          categories
+            .slice(0, 8)
+            .map((category) => (
+              <FilterPill
+                key={category.id}
+                title={category.name}
+                isSelected={selectedFilter === category.name}
+                onSelect={() =>
+                  setSelectedFilter(
+                    selectedFilter === category.name ? "" : category.name,
+                  )
+                }
+              />
+            ))}
+      </div>
+      <div className="grid grid-cols-[209px] justify-center gap-x-10 gap-y-4 md:grid-cols-[repeat(2,minmax(0,209px))] lg:grid-cols-[repeat(4,minmax(0,209px))]">
         {cats &&
           cats.map((cat) => {
-            return <Card cat={cat} />;
+            if (cat.breeds.length > 0) return <Card key={cat.id} cat={cat} />;
           })}
       </div>
-    </main>
+    </section>
   );
 }
