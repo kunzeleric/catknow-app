39027d65b2a65d123bfbfc3f8ba2d2bcd7787bc7
Eric Kunzel <kunzel.eric@gmail.com>
2024-11-02 14:04:22 -0300

    feat: included query provider in app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import { queryClient } from "@/lib/react-query";
 import "@/styles/globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.className}`}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </main>
+  );
 }
