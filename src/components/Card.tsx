4e77afc4aab4ffb0e7e4fc4ac4390ff2ef65bef6
Eric Kunzel <kunzel.eric@gmail.com>
2024-11-05 10:04:41 -0300

    refactor: turned card dv into link component with blank target

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,17 @@
 import { CatProps } from "@/@types/cat";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface CardProps {
   cat: CatProps;
 }
 
 export function Card({ cat }: CardProps) {
-  const { push } = useRouter();
   return (
-    <div
-      onClick={() => push(`/details/${cat.id}`)}
-      className="flex h-60 w-[209px] cursor-pointer flex-col rounded-xl border border-black"
+    <Link
+      href={`/details/${cat.id}`}
+      target="_blank"
+      className="flex h-60 w-[209px] cursor-pointer flex-col rounded-xl border border-black shadow-md"
     >
       <Image
         className="h-[209px] w-[209px] rounded-t-xl object-cover"
@@ -23,6 +23,6 @@ export function Card({ cat }: CardProps) {
       <p className="flex h-[31px] items-center justify-center text-center font-bold text-black">
         {cat.breeds?.[0]?.name}
       </p>
-    </div>
+    </Link>
   );
 }
